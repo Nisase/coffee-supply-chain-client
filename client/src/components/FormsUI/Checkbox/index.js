ce27b14972e6889eb5f229532cd9c23ea1cfbc4c
Nath <nathalia.barreirosf@gmail.com>
2022-05-02 23:23:30 -0500

    space added in comments according to esLint

--- client/src/components/FormsUI/Checkbox/index.js
+++ client/src/components/FormsUI/Checkbox/index.js
@@ -1,47 +1,39 @@
-import React from "react";
-import {
-	Checkbox,
-	FormControl,
-	FormControlLabel,
-	FormGroup,
-	FormLabel,
-} from "@mui/material";
-import { useField, useFormikContext } from "formik";
+import React from 'react';
+import { Checkbox, FormControl, FormControlLabel, FormGroup, FormLabel } from '@mui/material';
+import { useField, useFormikContext } from 'formik';
 
 const CheckboxWrapper = ({ name, label, legend, ...otherProps }) => {
-	const { setFieldValue } = useFormikContext();
+  const { setFieldValue } = useFormikContext();
 
-	const [field, meta] = useField(name);
+  const [field, meta] = useField(name);
 
-	const handleChange = (evt) => {
-		const { checked } = evt.target; //checked es un valor boolean
-		setFieldValue(name, checked);
-	};
+  const handleChange = (evt) => {
+    const { checked } = evt.target;
+    // checked es un valor boolean
+    setFieldValue(name, checked);
+  };
 
-	const configCheckbox = {
-		...field,
-		...otherProps,
-		onChange: handleChange,
-	};
+  const configCheckbox = {
+    ...field,
+    ...otherProps,
+    onChange: handleChange,
+  };
 
-	//como checkbox no tiene un prop for error necesito usar FormControl
-	const configFormControl = {};
+  // como checkbox no tiene un prop for error necesito usar FormControl
+  const configFormControl = {};
 
-	if (meta && meta.touched && meta.error) {
-		configFormControl.error = true;
-	}
+  if (meta && meta.touched && meta.error) {
+    configFormControl.error = true;
+  }
 
-	return (
-		<FormControl {...configFormControl}>
-			<FormLabel component="legend">{legend}</FormLabel>
-			<FormGroup>
-				<FormControlLabel
-					control={<Checkbox {...configCheckbox} />}
-					label={label}
-				/>
-			</FormGroup>
-		</FormControl>
-	);
+  return (
+    <FormControl {...configFormControl}>
+      <FormLabel component="legend">{legend}</FormLabel>
+      <FormGroup>
+        <FormControlLabel control={<Checkbox {...configCheckbox} />} label={label} />
+      </FormGroup>
+    </FormControl>
+  );
 };
 
 export default CheckboxWrapper;
