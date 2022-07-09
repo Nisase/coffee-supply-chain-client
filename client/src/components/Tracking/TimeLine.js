91850900e218b08531ab20562c4c79c476d9c09e
Ricardo Villarroel <ricardo.villarroel@epn.edu.ec>
2022-07-08 21:02:15 -0500

    time

--- client/src/components/Tracking/TimeLine.js
+++ client/src/components/Tracking/TimeLine.js
@@ -8,6 +8,8 @@ import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
 import TimelineDot from '@mui/lab/TimelineDot';
 
 import { Button, Grid, Typography, Tooltip } from '@mui/material';
+import PhaseCard from './PhaseCard';
+
 import { getCoffeERC20 } from '../../logic/erc20';
 import AskNextAction from '../../logic/GetNextAction/AskNextAction';
 import AskHarvest from '../../logic/GetHavest/AskHarvest';
@@ -19,6 +21,7 @@ import AskAgglom from '../../logic/GetAgglom/AskAgglom';
 
 
 const TimeLine = ({ batchNoIn }) => {
+  const [message, setMessage] = useState("Loading..");
   const [nextAction, setNextAction] = useState({});
   const [harversData, setHarversData] = useState({});
   const [processData, setProcessData] = useState({});
@@ -43,30 +46,73 @@ const TimeLine = ({ batchNoIn }) => {
       const erc = getCoffeERC20();
       const events = await erc.queryFilter(erc.filters.SetFarmDetails(null));
       const batchTemp = events.map((event) => event.args.batchNo);
-      const nextActionsTemp = batchTemp.map(async (item) => {
+      
+      /* const nextActionsTemp = batchTemp.map(async (item) => {
         const res = await AskNextAction({ batchNo: item });
         return res.data;
       });
-      
+      setBatchNo(batchTemp);
+      setNextActions(await Promise.all(nextActionsTemp));
+      */
+
       setNextAction(await AskNextAction({ batchNo:  batchNoIn}));
       setHarversData(await AskHarvest({ batchNo:  batchNoIn}));
       setProcessData(await AskProcess({ batchNo:  batchNoIn}));
       setInspectData(await AskGrainInspection({ batchNo:  batchNoIn}));
       setAgglomData(await AskAgglom({ batchNo:  batchNoIn}));
       // console.log(nextAction)
-      console.log(harversData)
+      console.log(processData)
+      if(nextAction && nextAction.data!=='DONE'){
+        setMessage("No Disponibles")
+      }
 
-      // setBatchNo(batchTemp);
-      // setNextActions(await Promise.all(nextActionsTemp));
+      
     };
     de()
   }, []);
 
+  function dateToYMD(dateIN) {
+    // eslint-disable-next-line no-self-compare
+    if(dateIN.getTime() !== dateIN.getTime()){
+      return "No disponible"
+    }
+    const d = dateIN.getDate()
+    const m = dateIN.getMonth() + 1; // Month from 0 to 11
+    const y = dateIN.getFullYear();
+    return `${d<9?`0${d}`:d}-${m+1<9?`0${m+1}`:m+1}-${y}`;
+}
+
   return (
       <Grid className='cursor-default relative'>
         <p className='text-center font-bold text-xl'>Tracking Coffe</p>
-        <p className='text-center my-4'>Bach #: <span className='underline '>{batchNoIn}</span></p>
-        <div style={{backgroundImage:"url(static/icons/data_shapes.svg)"}} className="w-full h-full absolute opacity-10"><br/></div>
+        <p className='text-center my-4 mb-10'>Bach #: <span className='underline '>{batchNoIn}</span></p>
+        <div style={{backgroundImage:"url(static/icons/data_shapes.svg)"}} className="w-full h-full absolute opacity-10 none"><br/></div>
+
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+        <PhaseCard title={"Cosecha"} className={"bg-green-100"} icon={"cosecha.png"} date={harversData.data ? dateToYMD(new Date(harversData.data.harvestDate)) : message}>
+              <div className='flex flex-col text-sm'>
+              <div className='flex flex-col'><div className='mt-0 mb-1 font-semibold'>Familia</div>{harversData.data ? harversData.data.coffeeFamily : message}</div>
+              <div className='flex flex-col'><div className='mt-5 mb-1 font-semibold'>Tipo de Semilla</div>{harversData.data ? harversData.data.typeOfSeed : message}</div>
+              <div className='flex flex-col'><div className='mt-5 mb-1 font-semibold'>Fertilizantes</div>{harversData.data ? harversData.data.fertilizerUsed : message}</div>
+              </div>
+        </PhaseCard>
+
+        <PhaseCard title={"Procesado"} className={"bg-red-100"} icon={"proccess.png"} date={processData.data ? dateToYMD(new Date(processData.data.millDate)) : message}>
+              <div className='flex flex-col text-sm'>
+              <div className='bg-gray-100 rounded-lg'><img src={processData.data ? processData.data.roastImageHash : ""} alt="Imagen del café Tostado" className='h-48 mx-auto'/></div>
+              <div className='flex flex-col'><div className='mt-5 mb-1 font-semibold'>Dirección del Procesado</div>{processData.data ? processData.data.procAddress : message}</div>
+              <div className='flex flex-col'><div className='mt-5 mb-1 font-semibold'>Fecha de Secado</div>{processData.data ? dateToYMD(new Date(processData.data.millDate)) : message}</div>
+              <div className='flex flex-col'><div className='mt-5 mb-1 font-semibold'>Tipo de Secado</div>{processData.data ? processData.data.typeOfDrying : message}</div>
+              <div className='flex flex-col'><div className='mt-5 mb-1 font-semibold'>Fecha de Tostado</div>{processData.data ? dateToYMD(new Date(processData.data.roastDate)) : message}</div>
+              <div className='flex flex-col'><div className='mt-5 mb-1 font-semibold'>Tipo de Tostado</div>{processData.data ? processData.data.typeOfRoast : message}</div>
+              <div className='flex flex-col'><div className='mt-5 mb-1 font-semibold'>Temp. de Tostado</div>{processData.data ? `${processData.data.roastTemp} grados` : message}</div>
+              <div className='bg-gray-200 p-2  mt-4 rounded-lg hover:cursor-pointer hover:underline text-center'>
+              <a href='http://localhost:3000/dashboard?batch=0x6B4964E34816C7FF32EA3787c2C615E583715197'>INGRESAR INFO</a>
+              </div>
+              </div>
+        </PhaseCard>
+        </div>
+
         <Timeline>
           <TimelineItem className="relative">
           <TimelineOppositeContent sx={{ m: 'auto 0' }} align="right" variant="body2">
