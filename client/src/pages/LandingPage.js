936b8dc26a41387f91b83e0aaf2be85401a0b4b8
Nath <nathalia.barreirosf@gmail.com>
2022-07-14 22:05:55 -0500

    LandingPage file deleted

--- client/src/pages/LandingPage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import HomeSection from '../components/HomePage/Home';
-import Page from '../components/Page';
-
-const LandingPage = () => {
-  return <div>LandingPage</div>;
-};
-
-export default LandingPage;
